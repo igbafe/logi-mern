044f3e55846a54a0d025ffd882e9ef0c13f20462
Igbafe Peter <igbafes8@gmail.com>
2025-06-25 22:02:01 +0100

    feat:desktop view

--- frontend/src/pages/TrackingGoods.jsx
+++ frontend/src/pages/TrackingGoods.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import Barcode from "../Components/Barcode";
+import { BsGlobe2 } from "react-icons/bs";
 
 function TrackingGoods() {
   const [code, setCode] = useState("");
@@ -20,6 +21,7 @@ function TrackingGoods() {
       if (res.data.success) {
         setData(res.data.data);
         setError("");
+        console.log("Tracking data:", res.data.data);
       } else {
         setError(res.data.message);
         setData(null);
@@ -64,27 +66,80 @@ function TrackingGoods() {
           {error && <p className="text-red-500">{error}</p>}
 
           {data && (
-            <div className="mt-6 border p-4 rounded shadow-lg max-w-md w-full text-left">
-              <h2 className="text-xl font-bold mb-2">Shipment Info</h2>
-              <p>
-                <strong>Tracking Code:</strong> {data.code}
-              </p>
-              <p>
-                <strong>Shipper:</strong> {data.shipper.name} -{" "}
-                {data.shipper.city}
-              </p>
-              <p>
-                <strong>Receiver:</strong> {data.receiver.name} -{" "}
-                {data.receiver.city}
-              </p>
-              <p>
-                <strong>Status:</strong> {data.shipment.status}
-              </p>
-              <p>
-                <strong>Weight:</strong> {data.shipment.weight}
-              </p>
-              <div className="mt-4">
+
+            <div className="mt-6 border px-24 py-10 rounded shadow-lg max-w-5xl w-full">
+              <div className="items-center justify-center flex flex-col mb-4">
+                <BsGlobe2 className="w-10 h-10 text-[#1e3a8a]" />
                 <Barcode code={data.code} />
+              </div>
+              <div className="flex justify-between gap-x-4 mb-10 text-gray-400">
+                <div className="w-full text-left flex flex-col gap-2">
+                  <h2 className="border-b-2 border-gray-400 text-left mb-6">Shipper Information</h2>
+                  <p> {data.shipper.name}</p>
+                  <p> {data.shipper.address}</p>
+                  <p> {data.shipper.phone}</p>
+                  <p> {data.shipper.email}</p>
+                </div>
+                <div className="w-full text-right flex flex-col gap-2">
+                  <h2 className="border-b-2 border-gray-400 text-right mb-6">Receiver Information</h2>
+                  <p>{data.receiver.name}</p>
+                  <p> {data.receiver.address}</p>
+                  <p> {data.receiver.phone}</p>
+                  <p> {data.receiver.email}</p>
+                </div>
+              </div>
+              <div className="w-full text-left flex flex-col  text-gray-400 gap-2">
+                <h2 className="border-b-2 border-gray-400 text-left mb-6">Shippment Information</h2>
+                <div className="flex justify-between">
+                  <div className="w-full flex  flex-col gap-2">
+                    <div>
+                      <p className="font-bold">Carrier</p>
+                      <p>{data.shipment.carrier}</p>
+                    </div>
+                    <div>
+                      <p className="font-bold">Carrier ID</p>
+                      <p>{data.shipment.carrierId}</p>
+                    </div>
+                    <div>
+                      <p className="font-bold">Destination</p>
+                      <p>{data.shipment.destination}</p>
+                    </div>
+                    <div>
+                      <p className="font-bold">Mode</p>
+                      <p>{data.shipment.mode}</p>
+                    </div>
+                    <div>
+                      <p className="font-bold">Origin</p>
+                      <p>{data.shipment.origin}</p>
+                    </div>
+                  </div>
+                  <div className="w-full flex flex-col gap-2">
+                    <div>
+                      <p className="font-bold">Payment Mode</p>
+                      <p>{data.shipment.paymentMode}</p>
+                    </div>
+                    <div>
+                      <p className="font-bold">Product</p>
+                      <p>{data.shipment.product}</p>
+                    </div>
+                    <div>
+                      <p className="font-bold">Pickup Date</p>
+                      <p>{data.shipment.pickupDate}</p>
+                    </div>
+                    <div>
+                      <p className="font-bold">Pickup Time</p>
+                      <p>{data.shipment.pickupTime}</p>
+                    </div>
+                    <div>
+                      <p className="font-bold">Quantity</p>
+                      <p>{data.shipment.quantity}</p>
+                    </div>
+                  </div>
+                  <div>
+                      <p className="font-bold">Weight</p>
+                      <p>{data.shipment.weight}</p>
+                    </div>
+                </div>
               </div>
             </div>
           )}
