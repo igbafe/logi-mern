dc1bea4878737559b0ca2d9cf4f1a788ee1deb04
Igbafe Peter <igbafes8@gmail.com>
2025-06-27 12:21:24 +0100

    production

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors());
 
@@ -21,6 +22,13 @@ app.use("/api/shipper", shipperRouter);
 app.use("/api/receiver", receiverRouter);
 app.use("/api/shippment", shipmentRouter);
 app.use("/api/barcode", barcodeRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
